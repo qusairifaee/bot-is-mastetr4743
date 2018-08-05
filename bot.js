d3a185f18f3dac2cffffde79102d08311f9343b0
qusairifaee <42096065+qusairifaee@users.noreply.github.com>
2018-08-05 16:56:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame(`قريبا أضافه كود ميوزك`,"http://twitch.tv/sfa7")
+  client.user.setGame(` مشغول اكل فشار`,"http://twitch.tv/sfa7")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
